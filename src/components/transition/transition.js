6fe530e2f994e203518a4994e19df882c825c4e9
Anthony <anthony@typehuman.com>
2020-07-13 21:09:51 +1000

    Try and fix sticky footer on SSR

--- src/components/transition/transition.js
+++ src/components/transition/transition.js
@@ -6,27 +6,24 @@ import { timeout } from 'constants/transition';
 class Transition extends PureComponent {
   render() {
     const { children, location } = this.props;
-
     const RoutesContainer = posed.div({
       enter: {
         opacity: 1,
         delay: timeout,
         delayChildren: timeout,
-        width: '100%',
-        height: '100%',
       },
       exit: {
         opacity: 0,
-        width: '100%',
-        height: '100%',
       },
     });
+
+    const containerStyle = { width: '100%', height: '100%' };
 
     // To enable page transitions on mount / initial load,
     // use the prop `animateOnMount={true}` on `PoseGroup`.
     return (
       <PoseGroup animateOnMount={true}>
-        <RoutesContainer key={location.pathname}>{children}</RoutesContainer>
+        <RoutesContainer style={containerStyle} key={location.pathname}>{children}</RoutesContainer>
       </PoseGroup>
     );
   }
