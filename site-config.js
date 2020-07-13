5e5991e626ebe47ceef94c52278f8379b7f5b591
Anthony <anthony@typehuman.com>
2020-07-13 21:22:20 +1000

    Update tagline

--- site-config.js
+++ site-config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   siteTitle: 'Anthony Cole',
   siteTitleShort: 'Anthony',
-  siteDescription: 'Creative Technologist who likes building cool things in Melbourne, Australia',
+  siteDescription: 'Senior Technologist in Melbourne, Australia',
   siteUrl: 'https://anthonycole.fyi',
   themeColor: '#000',
   backgroundColor: '#fff',
